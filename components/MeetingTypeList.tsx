1b4e1565a03299710347b8134a4957cff5a45a66
deepanshurwt06 <rwtd06@gmail.com>
2025-06-08 23:36:17 +0530

    build the home page front end

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+import MeetingModel from "./MeetingModel";
 
 const MeetingTypeList = () => {
-  return (
-   <section>
-    helo
-   </section>
-  )
-}
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduledMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
 
-export default MeetingTypeList
+  const createMeeting = () => {
+    
+  };
+
+  return (
+    <section className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        className="bg-[#FF742E]"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setMeetingState("isScheduledMeeting")}
+        className="bg-[#0e78f8]"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings"
+        handleClick={() => router.push("/recordings")}
+        className="bg-purple-500"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+        className="bg-[#F9A90E]"
+      />
+
+      <MeetingModel
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
